a65bdad30837657c6055dd4a7382aa578087376e
Aniket Gawas <aniketgawas109@gmail.com>
2024-01-27 18:38:38 +0530

    added static component for password change

---
+++ projects/calipharma/src/app/change-default-password/change-default-password.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-change-default-password',
+  templateUrl: './change-default-password.component.html',
+  styleUrls: ['./change-default-password.component.scss'],
+  standalone: true,
+})
+export class ChangeDefaultPasswordComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
